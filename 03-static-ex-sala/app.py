f526ab82128aae435d74b4dccb202d81b441f5e9
maduzuttion <aluno@ifc.blumenau.ifc.edu.br>
2024-04-11 09:16:30 -0300

    alteração 11/04

---
+++ 03-static-ex-sala/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")#rota para puxar o template home.html
+def home():
+    return render_template("home.html")
+
+@app.route("/about")#rota para puxar o template about.html
+def about():
+    return render_template("about.html")
+
+@app.route("/table")#rota para puxar o template table.html
+def table():
+    return render_template("table.html")
