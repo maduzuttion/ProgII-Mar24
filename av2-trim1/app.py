2065fcd83b3e0d928126fb86e6a9ce4eabe94069
maduzuttion <mariaezuttion@gmail.com>
2024-04-29 09:17:48 -0300

    commit 29/04 - 2

--- av2-trim1/app.py
+++ av2-trim1/app.py
@@ -13,3 +13,33 @@ class Produto(db.Entity):
 
     def __str__(self):
         return f'{self.nome_prod},{self.preco_prod}'
+
+@app.route("/")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/sobre")
+def about():
+    return render_template("sobre.html")
+
+@app.route("/listar_produto")
+def listar_prod():
+    with db_session:
+        produtos=Produto.select() 
+        return render_template("listar_prod.html", produtos=produtos)
+
+@app.route("/form_produto")
+def form_prod():
+    return render_template("form_prod.html")
+
+@app.route("/cadastrar_novo_produto")
+def cadastrar_novo_produto():
+    cod_bar=request.args.get("cod")
+    nome_prod= request.args.get("produto")
+    preco= request.args.get("preco")
+    qtdd= request.args.get("qtd")
+    tipo= request.args.get("tipo")
+    with db_session:
+        i= Produto(**request.args)
+        commit()
+        return redirect("listar_prod") 
