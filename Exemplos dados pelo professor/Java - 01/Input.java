861dc70b177d3fc28a3622dfc5c4cc1d9e4f57a2
maduzuttion <mariaezuttion@gmail.com>
2024-07-22 08:22:26 -0300

    att 22/07 - 01

--- Exemplos dados pelo professor/Java - 01/Input.java
+++ Exemplos dados pelo professor/Java - 01/Input.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-clas Input {
+class Input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Digite seu nome:");
-        String inp = input.nextline();
+        String inp = input.nextLine();
         System.out.print("Informe sua idade: ");
         int idade = input.nextInt();
         
-        System.out.printIn("Hello, " + inp + "sua idade é: " + idade)
+        System.out.println("Hello, " + inp + "sua idade é: " + idade);
     }
 }
