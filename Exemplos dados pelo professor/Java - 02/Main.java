5bf92c38d7992b3ae12ca911d36bf42361124258
maduzuttion <mariaezuttion@gmail.com>
2024-07-22 09:15:32 -0300

    att 22/07 - 01

--- Exemplos dados pelo professor/Java - 02/Main.java
+++ Exemplos dados pelo professor/Java - 02/Main.java
@@ -1,8 +1,24 @@
-public class Main {
-        public static void main(String[] args) {
-        System.out.println("Testes");
+//código 1:
+//public class Main {
+//        public static void main(String[] args) {
+//        System.out.println("Testes");
 
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+//        System.out.println(args[0]);
+ //       System.out.println(args[1]);
+//    }
+//}
+
+//For:
+public class Main{
+    public static void main (String[] args) {
+        System.out.println("Apenas Testes");
+
+        for (String lista : args){
+            System.out.println(lista);
+        }
+
+        for (int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
     }
 }
